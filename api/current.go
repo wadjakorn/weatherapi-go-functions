ba9109cb9209d740bc9454da99a175cfca571c5e
wadjakorn.t <wazjakorn@gmail.com>
2024-05-25 14:15:15 +0700

    fix: no cache set

--- api/current.go
+++ api/current.go
@@ -19,17 +19,17 @@ func Current(w http.ResponseWriter, r *http.Request) {
 	kvToken := "AX52ASQgNDU2ZDE5MTEtY2RlMC00MGI1LWFhMDYtZDRjNGNjYWI0OTA5MzgxYTU4MjQ1NWY1NDdiOTg4YWQ4NWUyZjMxM2M2OTQ="
 	getCacheReq, err := http.NewRequest(http.MethodGet, "https://enhanced-seal-32374.upstash.io/get/bangkok", nil)
 	if err != nil {
-		fmt.Fprintf(w, `<h1>Parse Get Cache URL Error: %s</h1>`, err.Error())
+		fmt.Printf(`Parse Get Cache URL Error: %s`, err.Error())
 	} else {
 		getCacheReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", kvToken))
 		cacheResp, err := client.Do(getCacheReq)
 
 		if err != nil {
-			fmt.Fprintf(w, `<h1>Get Cahce Error: %s</h1>`, err.Error())
+			fmt.Printf(`Get Cahce Error: %s`, err.Error())
 		} else if cacheResp.StatusCode == http.StatusOK {
 			b, err := io.ReadAll(cacheResp.Body)
 			if err != nil {
-				fmt.Fprintf(w, `<h1>Read Cache Body Error: %s</h1>`, err.Error())
+				fmt.Printf(`Read Cache Body Error: %s`, err.Error())
 			}
 
 			type kvResponse struct {
@@ -38,7 +38,7 @@ func Current(w http.ResponseWriter, r *http.Request) {
 			var parsedKvResp kvResponse
 			err = json.Unmarshal(b, &parsedKvResp)
 			if err != nil {
-				fmt.Fprintf(w, `<h1>Unmarshal Cache Body Error: %s</h1>`, err.Error())
+				fmt.Printf(`Unmarshal Cache Body Error: %s`, err.Error())
 			}
 
 			if parsedKvResp.Result != "" {
@@ -72,12 +72,12 @@ func Current(w http.ResponseWriter, r *http.Request) {
 		kvSetBody := bytes.NewReader([]byte(fmt.Sprintf(`["SET", "bangkok", "%s", "ex" ,"10"]`, respString)))
 		kvSeReq, err := http.NewRequest(http.MethodPost, "https://enhanced-seal-32374.upstash.io", kvSetBody)
 		if err != nil {
-			fmt.Fprintf(w, `<h1>Parse Set Cache URL Error: %s</h1>`, err.Error())
+			fmt.Printf(`Parse Set Cache URL Error: %s`, err.Error())
 		} else {
 			kvSeReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", kvToken))
 			_, err = client.Do(kvSeReq)
 			if err != nil {
-				fmt.Fprintf(w, `<h1>Create Cahce Error: %s</h1>`, err.Error())
+				fmt.Printf(`Create Cahce Error: %s`, err.Error())
 			}
 		}
 	}
