0fc917f1e34c8ce3077bd1704a5cad958e633bb8
wadjakorn.t <wazjakorn@gmail.com>
2024-05-26 22:34:08 +0700

    fix: improve response

--- api/bangkok.go
+++ api/bangkok.go
@@ -10,6 +10,12 @@ import (
 	"time"
 )
 
+type CustomResponse struct {
+	Result string   `json:"result"`
+	Errors []string `json:"errors"`
+	Cached bool     `json:"cached"`
+}
+
 func Bangkok(w http.ResponseWriter, r *http.Request) {
 	q := "bangkok"
 	url := "http://api.weatherapi.com/v1/current.json?key=b8768196a632446db6e52729242505&q=bangkok"
@@ -17,23 +23,20 @@ func Bangkok(w http.ResponseWriter, r *http.Request) {
 		Timeout: time.Second * 30,
 	}
 	var weatherObjectStr string
-	var errorString []string
-	isFromCache := false
+	cresp := CustomResponse{
+		Cached: false,
+		Errors: []string{},
+	}
 
 	defer func() {
-		var respStr string
-		if len(errorString) > 0 {
-			respStr = "<script>console.log('" + strings.Join(errorString, ",") + "');</script>"
-		}
-		htmlStr := fmt.Sprintf(`<script>console.log('cache:%v')</script><pre>%s</pre>`, isFromCache, weatherObjectStr)
-		respStr = respStr + htmlStr
-		fmt.Fprintf(w, "%s", respStr)
+		b, _ := json.Marshal(cresp)
+		fmt.Fprintf(w, "%s", string(b))
 	}()
 
 	kvToken := "AX52ASQgNDU2ZDE5MTEtY2RlMC00MGI1LWFhMDYtZDRjNGNjYWI0OTA5MzgxYTU4MjQ1NWY1NDdiOTg4YWQ4NWUyZjMxM2M2OTQ="
 	getCacheReq, err := http.NewRequest(http.MethodGet, "https://enhanced-seal-32374.upstash.io/get/"+q, nil)
 	if err != nil {
-		errorString = append(errorString, fmt.Sprintf(`Parse Get Cache URL Error: %s`, err.Error()))
+		cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Parse Get Cache URL Error: %s`, err.Error()))
 	} else {
 		getCacheReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", kvToken))
 		cacheResp, err := client.Do(getCacheReq)
@@ -43,7 +46,7 @@ func Bangkok(w http.ResponseWriter, r *http.Request) {
 		} else if cacheResp.StatusCode == http.StatusOK {
 			b, err := io.ReadAll(cacheResp.Body)
 			if err != nil {
-				errorString = append(errorString, fmt.Sprintf(`Read Cache Body Error: %s`, err.Error()))
+				cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Read Cache Body Error: %s`, err.Error()))
 			} else {
 				type kvResponse struct {
 					Result string `json:"result"`
@@ -51,32 +54,32 @@ func Bangkok(w http.ResponseWriter, r *http.Request) {
 				var parsedKvResp kvResponse
 				err = json.Unmarshal(b, &parsedKvResp)
 				if err != nil {
-					errorString = append(errorString, fmt.Sprintf(`Unmarshal Cache Body Error: %s`, err.Error()))
+					cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Unmarshal Cache Body Error: %s`, err.Error()))
 				}
 
 				if parsedKvResp.Result != "" {
 					weatherObjectStr = parsedKvResp.Result
-					isFromCache = true
+					cresp.Cached = true
 				}
 			}
 		}
 	}
 
-	if !isFromCache {
+	if !cresp.Cached {
 		resp, err := client.Get(url)
 		if err != nil {
-			errorString = append(errorString, fmt.Sprintf(`Client Get Error: %s`, err.Error()))
+			cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Client Get Error: %s`, err.Error()))
 			return
 		}
 
 		if resp.StatusCode != http.StatusOK {
-			errorString = append(errorString, fmt.Sprintf(`Client Get StatusCode: %d`, resp.StatusCode))
+			cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Client Get StatusCode: %d`, resp.StatusCode))
 			return
 		}
 
 		b, err := io.ReadAll(resp.Body)
 		if err != nil {
-			errorString = append(errorString, fmt.Sprintf(`Read Body Error: %s`, err.Error()))
+			cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Read Body Error: %s`, err.Error()))
 			return
 		}
 
@@ -87,12 +90,12 @@ func Bangkok(w http.ResponseWriter, r *http.Request) {
 		kvSetBody := bytes.NewReader([]byte(fmt.Sprintf(`["SET", %s, "%s", "ex" ,"60"]`, q, kvValue)))
 		kvSeReq, err := http.NewRequest(http.MethodPost, "https://enhanced-seal-32374.upstash.io", kvSetBody)
 		if err != nil {
-			errorString = append(errorString, fmt.Sprintf(`Parse Set Cache URL Error: %s`, err.Error()))
+			cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Parse Set Cache URL Error: %s`, err.Error()))
 		} else {
 			kvSeReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", kvToken))
 			_, err = client.Do(kvSeReq)
 			if err != nil {
-				errorString = append(errorString, fmt.Sprintf(`Create Cahce Error: %s`, err.Error()))
+				cresp.Errors = append(cresp.Errors, fmt.Sprintf(`Create Cahce Error: %s`, err.Error()))
 			}
 		}
 	}
